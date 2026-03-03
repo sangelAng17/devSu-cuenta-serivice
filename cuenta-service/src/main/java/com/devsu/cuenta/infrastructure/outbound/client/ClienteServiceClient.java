8cb1c0318f9e78899b771d11634733f27f6e046e
angel santiago hernandez <angel.hernandez@nuamx.com>
2026-03-02 21:17:36 -0500

    cuenta(fix) : Actualizacion de docker

--- cuenta-service/src/main/java/com/devsu/cuenta/infrastructure/outbound/client/ClienteServiceClient.java
+++ cuenta-service/src/main/java/com/devsu/cuenta/infrastructure/outbound/client/ClienteServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "cliente-service", url = "http://localhost:8080")
+@FeignClient(name = "cliente-service", url = "${cliente-service.url}")
 public interface ClienteServiceClient {
 
     @GetMapping("/Cliente/{id}")
